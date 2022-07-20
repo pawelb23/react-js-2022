fd4d9a0defe53d05c758edd522dddb7a16dd111b
pawelb23 <pawelbialek1823@gmail.com>
2022-07-20 20:02:41 +0200

    first commit

---
+++ react-tutorial-2022/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const WelcomeReact = () => (
+  <p className='font-size-welcome'>React Szkolenie</p>
+);
+
+function App() {
+  return (
+    <div className="App">
+<WelcomeReact />
+    </div>
+  );
+}
+
+export default App;
